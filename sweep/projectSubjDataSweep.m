c487834b7c13496b9a40fbfcd8a5aa97bfb5de99
Luca Lo Verde <luca.loverde0@gmail.com>
2021-06-14 14:20:54 -0700

    attempt to fix dimensionality problems with individual participants' projections

--- sweep/projectSubjDataSweep.m
+++ sweep/projectSubjDataSweep.m
@@ -4,10 +4,13 @@ function subj = projectSubjDataSweep(amp, phase, eAP)
 
     nCnd = size(amp, 1);    
     for c = 1:nCnd
-        subj.amp(:, :, c, :) = (cat(3, amp{c, :}));
-        subj.phase(:, :, c, :) = (cat(3, phase{c, :}));
+        % LLV not sure at all what "conditions" would be here, since each
+        % sub struct in rcRes are always relative to one condition at a
+        % time. Attempting to fix dimensionality
+        subj.amp(:, :, c, :, :) = permute((cat(5, amp{c, :})), [1, 2, 4, 3, 5]);
+        subj.phase(:, :, c, :, :) = permute((cat(5, phase{c, :})), [1, 2, 4, 3, 5]);
         % subj Err is transposed
 %         subj.err(:, :, c, :) = (cat(3, eAP{:, c}));
-        subj.err(:, :, c, :) = (cat(3, eAP{c, :}));
+        subj.err(:, :, c, :) = permute((cat(5, eAP{c, :})), [1, 2, 4, 3, 5])';
     end
 end
